b64208db81364ec7f649be3c847f7fab6b629e28
codemonkeyliuwei <673856629@qq.com>
2016-01-23 12:36:41 +0800

    power of two
    power of two

--- power_of_two.cpp
+++ power_of_two.cpp
@@ -5,11 +5,7 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0)
-        {
-            return false;
-        }
-        return (n > 0 && 0 == (n & (n-1)) ) ? true : false;
+        return (n > 0 && 0 == (n & (n-1)));
     }
 };
 
