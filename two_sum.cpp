47762e5587cb5a42af36d7db40cfb60bbd19a8f8
codemonkeyliuwei <673856629@qq.com>
2016-02-22 22:44:55 +0800

    two sum modify

--- two_sum.cpp
+++ two_sum.cpp
@@ -3,24 +3,23 @@
 #include <unordered_map>
 
 using namespace std;
-
-
+using namespace std::tr1;
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
-        vector<int> result(2, 0);
+        vector<int> result;
         unordered_map<int, int>::iterator it = hmap.begin();
-        for (int i = 0; i < numbers.size(); i++)
+        for (vector<int>::size_type i = 0; i < numbers.size(); i++)
         {
             it = hmap.find(target - numbers[i]);
             if (it != hmap.end())
             {
-                result[0] = it->second;
-                result[1] = i+1;
+                result.push_back(it->second);
+                result.push_back(i+1);
                 return result;
             }
             
-            hmap.insert(pair<int, int>(numbers[i], i+1));
+            hmap.insert(pair<int, int>(numbers[i], i));
         }
     }
 
