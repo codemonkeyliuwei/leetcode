91eeec795670dfeff5a931437da920faa9f62537
codemonkeyliuwei <673856629@qq.com>
2016-05-08 10:34:50 +0800

    rectangle area

---
+++ rectangle_area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        if ((E >= C) && (F >= D) && (A <= G) && (B <= H))
+    }
+};
+
+int main(void)
+{
+    Solution s;
+    cout << s.computeArea(-3, 0, 3, 4, 0, -1, 9, 2) << endl;
+}
